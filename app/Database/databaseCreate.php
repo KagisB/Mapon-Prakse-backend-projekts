64691e8d2fac7307c228f6ece5656105443dacb8
KagisB <kb16151@edu.lu.lv>
2022-06-06 11:55:12 +0300

    Remove vendor and .idea directory

--- app/Database/databaseCreate.php
+++ app/Database/databaseCreate.php
@@ -1,5 +1,5 @@
 <?php
-echo "Start file?";
+//echo "Start file?";
 //$servername = "172.17.0.2";
 $servername = "127.0.0.1";
 $username = "admin";
@@ -13,10 +13,10 @@ if($conn->error){
 }
 $sql = "CREATE DATABASE carRoute";
 if(mysqli_query($conn,$sql)){
-    echo "Database created successfully";
+    //echo "Database created successfully";
     //echo "Created database?";
 }else {
-    echo "Error creating database: " . mysqli_error($conn);
+    //echo "Error creating database: " . mysqli_error($conn);
 }
 mysqli_close($conn);
 //echo "Got past database creation?";
@@ -29,10 +29,10 @@ $sql = "CREATE TABLE Users(
 )";
 //echo "Created table query?";
 if($conn->query($sql)===TRUE){
-    echo "Table Users created successfully";
+    //echo "Table Users created successfully";
 }
 else{
-    echo "Error creating table: " . $conn->error;
+    //echo "Error creating table: " . $conn->error;
 }
 mysqli_close($conn);
 //echo "Closed connection?";
